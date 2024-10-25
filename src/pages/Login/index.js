aff9bc79dee714fc3829dbd761956f20b798e51a
2unhi <ironhak1106@gmail.com>
2024-10-26 03:19:47 +0900

    [design] 로그인페이지 디자인

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -20,34 +20,32 @@ const LoginPage = () => {
   };
 
   return (
-    <div className="mx-auto flex h-screen w-[450px] flex-col items-center justify-center bg-primary-50">
-      <div className="p-4 text-center">
-        <h1 className="mb-8 font-paperlogy-heading text-[40px] text-white">
-          복숭아멘토
-        </h1>
-        <div className="mb-8">
-          <LottieAnimation />
-        </div>
-        <div className="flex flex-col items-center">
-          <img
-            src="/images/kakao_login.svg"
-            alt="Kakao Login"
-            className="w-[320px] h-[60px] cursor-pointer hover:opacity-90 transition-opacity"
-            onClick={handleKakaoLoginClick}
-          />
-          <img
-            src="/images/naver_login.svg"
-            alt="Naver Login"
-            className="w-[320px] h-[60px] cursor-pointer hover:opacity-90 transition-opacity"
-            onClick={handleNaverLoginClick}
-          />
-          <img
-            src="/images/google_login.svg"
-            alt="Google Login"
-            className="w-[320px] h-[60px] cursor-pointer hover:opacity-90 transition-opacity"
-            onClick={handleGoogleLoginClick}
-          />
-        </div>
+    <div className="mx-auto flex h-full w-full max-w-[500px] flex-col items-center justify-center px-6 bg-primary-50 shadow-lg">
+      <h1 className="mb-8 font-paperlogy-heading text-5xl text-white">
+        복숭아멘토
+      </h1>
+      <div className="mb-12">
+        <LottieAnimation />
+      </div>
+      <div className="flex flex-col items-center">
+        <img
+          src="/images/kakao_login.svg"
+          alt="Kakao Login"
+          className="w-full max-w-[360px] h-[60px] cursor-pointer hover:opacity-90 transition-opacity"
+          onClick={handleKakaoLoginClick}
+        />
+        <img
+          src="/images/naver_login.svg"
+          alt="Naver Login"
+          className="w-full max-w-[360px] h-[60px] cursor-pointer hover:opacity-90 transition-opacity"
+          onClick={handleNaverLoginClick}
+        />
+        <img
+          src="/images/google_login.svg"
+          alt="Google Login"
+          className="w-full max-w-[360px] h-[60px] cursor-pointer hover:opacity-90 transition-opacity"
+          onClick={handleGoogleLoginClick}
+        />
       </div>
     </div>
   );
