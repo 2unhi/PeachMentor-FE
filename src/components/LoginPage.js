31cb3493f6f4f4fa4e1a611db1bfc77127e4cd9a
2unhi <ironhak1106@gmail.com>
2024-10-25 14:57:43 +0900

    [design] 소셜 로그인 이미지 수정

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -1,52 +1,51 @@
-import React from 'react';
-import axios from 'axios';
-import '../css/LoginPage.css';
+import React from "react";
+import axios from "axios";
+import "../css/LoginPage.css";
 import Record from "./Record";
-import {SPRING_API_URL} from "../constants";
+import { SPRING_API_URL } from "../constants";
 
 axios.defaults.withCredentials = true;
 
-const LoginPage = message => {
+const LoginPage = (message) => {
+  // OAuth2 로그인
+  const handleKakaoLoginClick = () => {
+    window.location.href = `${SPRING_API_URL}/oauth/kakao`;
+  };
 
-    // OAuth2 로그인
-    const handleKakaoLoginClick = () => {
-        window.location.href = `${SPRING_API_URL}/oauth/kakao`;
-    };
+  const handleNaverLoginClick = () => {
+    window.location.href = `${SPRING_API_URL}/oauth/naver`;
+  };
 
-    const handleNaverLoginClick = () => {
-        window.location.href = `${SPRING_API_URL}/oauth/naver`;
-    };
+  const handleGoogleLoginClick = () => {
+    window.location.href = `${SPRING_API_URL}/oauth/google`;
+  };
 
-    const handleGoogleLoginClick = () => {
-        window.location.href = `${SPRING_API_URL}/oauth/google`;
-    };
-
-    return (
-        <div className="login-container">
-            <h2>복숭아 멘토~</h2>
-            <div className="social-login-buttons">
-                <img
-                    src="/images/kakao-icon.png"
-                    alt="Kakao Login"
-                    className="social-icon"
-                    onClick={handleKakaoLoginClick}
-                />
-                <img
-                    src="/images/naver-icon.png"
-                    alt="Naver Login"
-                    className="social-icon"
-                    onClick={handleNaverLoginClick}
-                />
-                <img
-                    src="/images/google-icon.png"
-                    alt="Google Login"
-                    className="social-icon"
-                    onClick={handleGoogleLoginClick}
-                />
-                <Record /> {/* Record 컴포넌트 추가 */}
-            </div>
-        </div>
-    );
+  return (
+    <div className="login-container">
+      <h2>복숭아 멘토~</h2>
+      <div className="social-login-buttons">
+        <img
+          src="/images/kakao_login.svg"
+          alt="Kakao Login"
+          className="social-icon"
+          onClick={handleKakaoLoginClick}
+        />
+        <img
+          src="/images/naver_login.svg"
+          alt="Naver Login"
+          className="social-icon"
+          onClick={handleNaverLoginClick}
+        />
+        <img
+          src="/images/google_login.svg"
+          alt="Google Login"
+          className="social-icon"
+          onClick={handleGoogleLoginClick}
+        />
+        <Record /> {/* Record 컴포넌트 추가 */}
+      </div>
+    </div>
+  );
 };
 
 export default LoginPage;
