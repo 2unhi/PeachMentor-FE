902992f9a9f56ac4299a6520ac05bba3aa5b3f5c
2unhi <ironhak1106@gmail.com>
2024-10-25 16:43:20 +0900

    [design] Lottie 이미지 추가 및 기초 레이아웃

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -1,12 +1,12 @@
 import React from "react";
 import axios from "axios";
-import Record from "./Record";
+import LottieAnimation from "./LottieAnimation";
 import { SPRING_API_URL } from "../constants";
 
 axios.defaults.withCredentials = true;
 
-const LoginPage = (message) => {
-  // OAuth2 로그인
+const LoginPage = () => {
+  // OAuth2 소셜 로그인
   const handleKakaoLoginClick = () => {
     window.location.href = `${SPRING_API_URL}/oauth/kakao`;
   };
@@ -20,28 +20,34 @@ const LoginPage = (message) => {
   };
 
   return (
-    <div className="flex flex-col justify-center items-center h-screen text-center p-4">
-      <h2 className="mb-8 text-2xl font-bold">복숭아 멘토~</h2>
-      <div className="flex flex-col items-center">
-        <img
-          src="/images/kakao_login.svg"
-          alt="Kakao Login"
-          className="w-48 mb-4 cursor-pointer"
-          onClick={handleKakaoLoginClick}
-        />
-        <img
-          src="/images/naver_login.svg"
-          alt="Naver Login"
-          className="w-48 mb-4 cursor-pointer"
-          onClick={handleNaverLoginClick}
-        />
-        <img
-          src="/images/google_login.svg"
-          alt="Google Login"
-          className="w-48 mb-4 cursor-pointer"
-          onClick={handleGoogleLoginClick}
-        />
-        <Record /> {/* Record 컴포넌트 추가 */}
+    <div className="mx-auto flex h-screen w-[450px] flex-col items-center justify-center bg-primary-50">
+      <div className="p-4 text-center">
+        <h1 className="mb-8 font-paperlogy-heading text-[40px] text-white">
+          복숭아멘토
+        </h1>
+        <div className="mb-8">
+          <LottieAnimation />
+        </div>
+        <div className="flex flex-col items-center">
+          <img
+            src="/images/kakao_login.svg"
+            alt="Kakao Login"
+            className="w-[320px] h-[60px] cursor-pointer hover:opacity-90 transition-opacity"
+            onClick={handleKakaoLoginClick}
+          />
+          <img
+            src="/images/naver_login.svg"
+            alt="Naver Login"
+            className="w-[320px] h-[60px] cursor-pointer hover:opacity-90 transition-opacity"
+            onClick={handleNaverLoginClick}
+          />
+          <img
+            src="/images/google_login.svg"
+            alt="Google Login"
+            className="w-[320px] h-[60px] cursor-pointer hover:opacity-90 transition-opacity"
+            onClick={handleGoogleLoginClick}
+          />
+        </div>
       </div>
     </div>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,7 @@
 const defaultTheme = require("tailwindcss/defaultTheme");
 
 module.exports = {
-  content: [
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "!./node_modules",
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}", "!./node_modules"],
   theme: {
     extend: {
       colors: {
