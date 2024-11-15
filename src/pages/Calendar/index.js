5c895682133fbaa5f412538cf0f9a0a389072463
2unhi <ironhak1106@gmail.com>
2024-11-15 16:14:39 +0900

    [feat] react calendar 기능 추가

--- src/pages/Calendar/index.js
+++ src/pages/Calendar/index.js
@@ -1,18 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "../../components/Header";
 import NavBar from "../../components/NavBar";
+import Calendar from "react-calendar";
+import "react-calendar/dist/Calendar.css";
 
-const Calendar = () => {
+const CalendarPage = () => {
+  const [selectedDate, setSelectedDate] = useState(new Date());
+
+  const handleDateChange = (date) => {
+    setSelectedDate(date);
+  };
+
   return (
     <div className="w-full h-full max-w-[500px] mx-auto flex flex-col bg-[#fcfcfc]">
       <Header />
-      {/* 이후 캘린터 페이지 컨텐츠 수정 예정 */}
-      <main className="flex-grow flex flex-col items-center justify-center p-4">
-        <h1 className="text-xl font-bold text-primary-900">캘린더 페이지</h1>
-      </main>
+      <div className="flex flex-col items-center flex-grow p-4">
+        <div className="flex justify-center items-center w-full py-4 mb-[-2px]">
+          <h1 className="text-2xl font-bold text-grayscale-90">나의 기록</h1>
+        </div>
+
+        <div className="w-full max-w-[400px] rounded-xl">
+          <Calendar
+            onChange={handleDateChange} // 날짜 선택 시 상태 업데이트
+            value={selectedDate} // 선택된 날짜를 상태로 관리
+            className="w-full"
+            tileClassName={({ date, view }) =>
+              date.getDate() === selectedDate.getDate() &&
+              date.getMonth() === selectedDate.getMonth() &&
+              date.getFullYear() === selectedDate.getFullYear()
+                ? "bg-primary-100 text-white font-semibold rounded-full"
+                : "hover:bg-gray-100 rounded-full transition duration-150"
+            }
+            formatDay={(locale, date) => (
+              <div className="flex items-center justify-center">
+                <span className="text-sm font-medium">{date.getDate()}</span>
+              </div>
+            )}
+          />
+        </div>
+      </div>
       <NavBar />
     </div>
   );
 };
 
-export default Calendar;
+export default CalendarPage;
