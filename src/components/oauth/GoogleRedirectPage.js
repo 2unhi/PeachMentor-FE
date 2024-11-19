190dcbf68ed4822ee2eb52f02d5092ae28214652
2unhi <ironhak1106@gmail.com>
2024-11-19 16:23:15 +0900

    [design] 소셜 로그인 직후 나오는 로딩 페이지 기본 작업

--- src/components/oauth/GoogleRedirectPage.js
+++ src/components/oauth/GoogleRedirectPage.js
@@ -1,50 +1,49 @@
-import React, {useEffect} from "react";
-import {useLocation, useNavigate} from "react-router-dom";
+import React, { useEffect } from "react";
+import LoginProcessingPage from "../LoginProcessing";
+import { useLocation, useNavigate } from "react-router-dom";
 import axios from "axios";
-import {SPRING_API_URL} from "../../constants/api";
+import { SPRING_API_URL } from "../../constants/api";
 
 axios.defaults.withCredentials = true;
 
 const GoogleRedirectPage = () => {
-    const location = useLocation();
-    const navigate = useNavigate();
+  const location = useLocation();
+  const navigate = useNavigate();
 
-    useEffect(() => {
-        const handleOAuthGoogle = async (code) => {
-            try {
-                const response = await axios.get(
-                    `${SPRING_API_URL}/oauth/login/google?code=${code}`,
-                    {}
-                );
-                if (response.data.isSuccess) {
-                    const accessToken =
-                        response.headers["Authorization"] ||
-                        response.headers["authorization"];
-                    const role = response.data.result;
-                    localStorage.setItem("accessToken", accessToken);
+  useEffect(() => {
+    const handleOAuthGoogle = async (code) => {
+      try {
+        const response = await axios.get(
+          `${SPRING_API_URL}/oauth/login/google?code=${code}`,
+          {}
+        );
+        if (response.data.isSuccess) {
+          const accessToken =
+            response.headers["Authorization"] ||
+            response.headers["authorization"];
+          const role = response.data.result;
+          localStorage.setItem("accessToken", accessToken);
 
-                    if (role === "GUEST")
-                        navigate("/guestrecord");
-                    else if (role === "USER")
-                        navigate("/main");
-                } else {
-                    console.error("OAuth2 로그인 오류");
-                    console.log(response.data.code);
-                    console.log(response.data.message);
-                }
-            } catch (error) {
-                console.error("로그인 실패", error);
-            }
-        };
-
-        const searchParams = new URLSearchParams(location.search);
-        const code = searchParams.get("code");
-        if (code) {
-            handleOAuthGoogle(code);
+          if (role === "GUEST") navigate("/guestrecord");
+          else if (role === "USER") navigate("/main");
+        } else {
+          console.error("OAuth2 로그인 오류");
+          console.log(response.data.code);
+          console.log(response.data.message);
         }
-    }, [location, navigate]); // 의존성 배열에서 navigate 추가
+      } catch (error) {
+        console.error("로그인 실패", error);
+      }
+    };
 
-    return <div>Processing...</div>;
+    const searchParams = new URLSearchParams(location.search);
+    const code = searchParams.get("code");
+    if (code) {
+      handleOAuthGoogle(code);
+    }
+  }, [location, navigate]); // 의존성 배열에서 navigate 추가
+
+  return <LoginProcessingPage />;
 };
 
 export default GoogleRedirectPage;
