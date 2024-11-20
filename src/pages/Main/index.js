159c7e9d9196e0d8530d17c9de1e350814439cd0
Hyun0828 <spdlqj4818@naver.com>
2024-11-20 22:17:20 +0900

    [fix] useLocation 추가

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -5,7 +5,7 @@ import Header from "../../components/Header";
 import NavBar from "../../components/NavBar";
 import {SPRING_API_URL} from "../../constants/api";
 import instance from "../../axios/TokenInterceptor";
-import {useNavigate} from "react-router-dom";
+import {useLocation, useNavigate} from "react-router-dom";
 import SelfFeedback from "./components/SelfFeedback";
 
 import "./style/button.css";
