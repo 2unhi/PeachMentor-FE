dcc7891afb343abddd4fe28c4fe41c8acba05314
Hyun <71479783+Hyun0828@users.noreply.github.com>
2024-11-20 17:50:53 +0900

    [fix] 메인 페이지에서 컴포넌트 중복되는 현상 제거 (#29)
    * [fix] 메인 페이지에서 컴포넌트 중복되는 현상 제거
    
    * [fix] 가운데 정렬 및 하루 1회 스피치 제한 해제
    
    * [fix] 메인 페이지 컴포넌트 분할하기
    
    * [feat] Header와 로그인 시에도 main url에 파라미터 추가
    
    * [feat] 파라미터 전달 문제 해결
    
    * [feat] 로그인 시 메인페이지로 이동 로직
    
    * [feat] 로그인 시 메인페이지로 이동 로직
    
    * [feat] 로그인 시 메인페이지로 이동 로직 수정
    
    * [feat] Promise로 비동기 작업 완료
    
    * [feat] Promise로 비동기 작업 후 useEffect 처리
    
    * [feat] 로그인 후 메인페이지로 넘어갈 때 상태가 아니라 지역변수를 통해 전달
    
    * [fix] NavBar Button tag 분리
    
    * [feat] 로그인 로딩 창 애니메이션 추가

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,38 +1,92 @@
 import React from "react";
-import { useNavigate } from "react-router-dom";
+import {useNavigate} from "react-router-dom";
+import instance from "../axios/TokenInterceptor";
+import {SPRING_API_URL} from "../constants/api";
 
 const NavBar = () => {
-  const navigate = useNavigate();
+    const navigate = useNavigate();
 
-  // 하단 바 버튼 경로, 이미지, 텍스트를 저장하는 배열
-  const menuItems = [
-    { path: "/calendar", icon: "/webp/calendar_button.webp", label: "캘린더" },
-    { path: "/main", icon: "/webp/home_button.webp", label: "홈" },
-    {
-      path: "/statistics",
-      icon: "/webp/statistics_button.webp",
-      label: "통계",
-    },
-  ];
+    const handleMainButton = async () => {
+        let isCompleteSpeech = false;
+        let selfFeedback = null;
 
-  return (
-    <nav className="bottom-0 left-0 w-full max-w-[500px] mx-auto flex justify-around border-t border-slate-500 py-3">
-      {menuItems.map((item, index) => (
-        <button
-          key={index}
-          onClick={() => navigate(item.path)}
-          className="flex flex-col items-center"
+        try {
+            const response = await instance.get(`${SPRING_API_URL}/answers/completions`)
+            if (response.data.isSuccess) {
+                if (response.data.code === "ANSWER4001" || response.data.code === "USER4002" || response.data.code === "ACCESSTOKEN4002") {
+                    console.error("오늘 답변 했는 지 여부 받아오기 API 서버 에러");
+                } else {
+                    if (response.data.result.answerExists) {
+                        isCompleteSpeech = true;
+                    } else {
+                        isCompleteSpeech = false;
+                    }
+                    console.log("오늘 답변 했는 지 여부 받아오기 성공");
+                }
+            } else {
+                console.error("오늘 답변 했는 지 여부 받아오기 실패");
+            }
+        } catch (error) {
+            console.error("오늘 답변 했는 지 여부 받아오기 실패");
+        }
+        try {
+            const response = await instance.get(`${SPRING_API_URL}/self-feedbacks/latest-feedbacks`);
+            if (response.data.isSuccess) {
+                if (response.data.code === "ANSWER4001" || response.data.code === "SELFFEEDBACK4001") {
+                } else {
+                    selfFeedback = response.data.result.feedback;
+                    console.log("이전 셀프 피드백 받아오기 성공");
+                }
+            } else {
+                console.error("이전 셀프 피드백 받아오기 실패");
+            }
+        } catch (error) {
+            console.error("이전 셀프 피드백 받아오기 실패");
+        }
+        navigate(`/main?selfFeedback=${selfFeedback}&isCompleteSpeech=${isCompleteSpeech}`);
+    }
+
+    return (
+        <nav
+            className="bottom-0 left-0 w-full max-w-[500px] mx-auto flex justify-around border-t border-slate-500 py-3"
         >
-          <img
-            src={item.icon}
-            alt={`${item.label} Icon`}
-            className="w-6 h-6 mb-1"
-          />
-          <span className="text-base font-paperlogy-title">{item.label}</span>
-        </button>
-      ))}
-    </nav>
-  );
+            <button
+                onClick={() => navigate("/calendar")}
+                className="flex flex-col items-center"
+            >
+                <img
+                    src="/webp/calendar_button.webp"
+                    alt="캘린더 Icon"
+                    className="w-6 h-6 mb-1"
+                />
+                <span className="text-base font-paperlogy-title">캘린더</span>
+            </button>
+
+            <button
+                onClick={handleMainButton}
+                className="flex flex-col items-center"
+            >
+                <img
+                    src="/webp/home_button.webp"
+                    alt="홈 Icon"
+                    className="w-6 h-6 mb-1"
+                />
+                <span className="text-base font-paperlogy-title">홈</span>
+            </button>
+
+            <button
+                onClick={() => navigate("/statistics")}
+                className="flex flex-col items-center"
+            >
+                <img
+                    src="/webp/statistics_button.webp"
+                    alt="통계 Icon"
+                    className="w-6 h-6 mb-1"
+                />
+                <span className="text-base font-paperlogy-title">통계</span>
+            </button>
+        </nav>
+    );
 };
 
 export default NavBar;
