06ee3b06509f20199aef7f5874f9afc01bd85327
Hyun0828 <spdlqj4818@naver.com>
2024-11-21 18:08:22 +0900

    [error] 깃허브 액션 에러

--- src/pages/Main/components/SelfFeedback.js
+++ src/pages/Main/components/SelfFeedback.js
@@ -17,15 +17,6 @@ const SelfFeedback = ({selfFeedback}) => {
             </div>
         </div>
     );
-
-    // return (
-    //     <div className="p-6 bg-white border-2 rounded-lg shadow-md border-primary-40">
-    //         <div
-    //             className="p-4 text-lg font-medium text-center font-paperlogy-title text-grayscale-100">
-    //             {formatContent(selfFeedback)}
-    //         </div>
-    //     </div>
-    // );
 };
 
 export default SelfFeedback;
