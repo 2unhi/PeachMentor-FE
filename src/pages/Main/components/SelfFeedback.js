fea7d8539716968026b5097e14b9519972800a40
2unhi <ironhak1106@gmail.com>
2024-11-21 23:13:47 +0900

    [design] 메인 홈에서 보여지는 이전 셀프 피드백 문구 디자인 수정

--- src/pages/Main/components/SelfFeedback.js
+++ src/pages/Main/components/SelfFeedback.js
@@ -9,9 +9,13 @@ const SelfFeedback = ({ selfFeedback }) => {
       </React.Fragment>
     ));
   };
+
   return (
-    <div className="w-[80%] p-6 bg-grayscale-00 border-2 rounded-lg border-grayscale-10">
-      <div className="p-4 text-lg font-medium text-center font-paperlogy-title text-grayscale-100">
+    <div className="w-[80%] p-6 bg-grayscale-05 border-2 rounded-lg border-grayscale-10">
+      <div className="mb-2 text-xl font-bold text-center font-paperlogy-title text-grayscale-100">
+        셀프 피드백이 도착했어요
+      </div>
+      <div className="p-4 text-lg font-medium text-center font-paperlogy-title text-grayscale-190">
         {formatContent(selfFeedback)}
       </div>
     </div>
