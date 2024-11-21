b72cb693d1af0e8e9406b9e18f366b1c3574bb51
Hyun0828 <spdlqj4818@naver.com>
2024-11-21 18:06:44 +0900

    [fix] SelfFeedback 박스 크기 조절

--- src/pages/Main/components/SelfFeedback.js
+++ src/pages/Main/components/SelfFeedback.js
@@ -10,15 +10,22 @@ const SelfFeedback = ({selfFeedback}) => {
             </React.Fragment>
         ));
     };
-
     return (
-        <div className="p-6 bg-white border-2 rounded-lg shadow-md border-primary-40">
-            <div
-                className="p-4 text-lg font-medium text-center font-paperlogy-title text-grayscale-100">
+        <div className="w-[80%] p-6 bg-white border-2 rounded-lg shadow-md border-primary-40">
+            <div className="p-4 text-lg font-medium text-center font-paperlogy-title text-grayscale-100">
                 {formatContent(selfFeedback)}
             </div>
         </div>
     );
+
+    // return (
+    //     <div className="p-6 bg-white border-2 rounded-lg shadow-md border-primary-40">
+    //         <div
+    //             className="p-4 text-lg font-medium text-center font-paperlogy-title text-grayscale-100">
+    //             {formatContent(selfFeedback)}
+    //         </div>
+    //     </div>
+    // );
 };
 
 export default SelfFeedback;
