0cfc368e327f2fb184ba3c219aa9e7475efa7c0b
2unhi <ironhak1106@gmail.com>
2024-12-05 22:30:35 +0900

    [fix] 충돌 오류 해결

--- src/pages/Calendar/index.js
+++ src/pages/Calendar/index.js
@@ -1,192 +1,124 @@
-import React, {useContext, useEffect, useState} from "react";
+import React, { useContext, useEffect, useState } from "react";
 import Header from "../../components/Header";
 import NavBar from "../../components/NavBar";
 import Calendar from "react-calendar";
 import "react-calendar/dist/Calendar.css";
 import instance from "../../axios/TokenInterceptor";
-import {SPRING_API_URL} from "../../constants/api";
-import {useNavigate} from "react-router-dom";
-import {NotificationContext} from "../../context/NotificationProvider";
+import { SPRING_API_URL } from "../../constants/api";
+import { useNavigate } from "react-router-dom";
+import { NotificationContext } from "../../context/NotificationProvider";
 
 const CalendarPage = () => {
-    const navigate = useNavigate();
-    const [selectedDate, setSelectedDate] = useState(new Date());
-    const [markedDates, setMarkedDates] = useState(new Array(32).fill(null));
+  const navigate = useNavigate();
+  const [selectedDate, setSelectedDate] = useState(new Date());
+  const [markedDates, setMarkedDates] = useState(new Array(32).fill(null));
 
-    const {
-        isNewReport,
-        setIsNewReport,
-    } = useContext(NotificationContext);
+  const { isNewReport, setIsNewReport } = useContext(NotificationContext);
 
-    const handleDateChange = (date) => {
-        setSelectedDate(date);
+  const handleDateChange = (date) => {
+    setSelectedDate(date);
 
-        const answerId = markedDates[date.getDate() - 1].answerId;
-        if (answerId > 0) {
-            const newDate = new Date(date);
-            newDate.setDate(newDate.getDate() + 1);
-            const formattedDate = newDate.toISOString().split("T")[0];
-            navigate(`/feedback?answerId=${answerId}&date=${formattedDate}`);
-        } else {
-            console.log("해당 날짜에 스피치를 진행하지 않았습니다.");
-        }
-    };
-
-    const fetchCalendarData = async (year, month) => {
-        try {
-            const response = await instance.get(`${SPRING_API_URL}/calendars`, {
-                params: {year, month},
-            });
-            if (response.data.isSuccess) {
-                const updatedDates = new Array(32).fill(null); // 초기화
-                response.data.result.forEach((answerId, index) => {
-                    if (answerId !== 0) {
-                        const fullDate = new Date(year, month - 1, index + 1); // 날짜 계산
-                        updatedDates[index] = {date: fullDate, answerId}; // 날짜와 answerId를 객체로 저장
-                    }
-                });
-                console.log(updatedDates);
-                setMarkedDates(updatedDates);
-                console.log("달력 정보 가져오기 성공");
-            } else {
-                console.error("달력 정보 가져오기 실패", response.data.message);
-            }
-        } catch (error) {
-            console.error("달력 정보 가져오기 요청 오류", error);
-        }
-    };
-
-    useEffect(() => {
-        const year = selectedDate.getFullYear();
-        const month = selectedDate.getMonth() + 1;
-        fetchCalendarData(year, month);
-    }, [selectedDate]);
-
-    const getTileClassName = ({date, view}) => {
-        const today = new Date();
-        const isWeekend = date.getDay() === 0 || date.getDay() === 6; // 주말 (0: 일요일, 6: 토요일)
-
-        // 오늘 날짜
-        if (view === "month") {
-            if (
-                date.getDate() === today.getDate() &&
-                date.getMonth() === today.getMonth() &&
-                date.getFullYear() === today.getFullYear()
-            ) {
-                return "text-pink-700 font-semibold";
-            }
-            // 선택된 날짜
-            if (
-                date.getDate() === selectedDate.getDate() &&
-                date.getMonth() === selectedDate.getMonth() &&
-                date.getFullYear() === selectedDate.getFullYear()
-            ) {
-                return "font-semibold";
-            }
-            if (isWeekend) {
-                return "text-black"; // 검정색 텍스트
-            }
-        }
-        return "hover:bg-grayscale-20 transition";
-    };
-
-
-    const handleCloseNotification = () => {
-        setIsNewReport(false);
+    const answerId = markedDates[date.getDate() - 1]?.answerId; // 안전하게 데이터 접근
+    if (answerId > 0) {
+      const newDate = new Date(date);
+      newDate.setDate(newDate.getDate() + 1);
+      const formattedDate = newDate.toISOString().split("T")[0];
+      navigate(`/feedback?answerId=${answerId}&date=${formattedDate}`);
+    } else {
+      console.log("해당 날짜에 스피치를 진행하지 않았습니다.");
     }
+  };
 
-    return (
-        <div className="w-full h-full max-w-[500px] mx-auto flex flex-col bg-white">
+  const fetchCalendarData = async (year, month) => {
+    try {
+      const response = await instance.get(`${SPRING_API_URL}/calendars`, {
+        params: { year, month },
+      });
+      if (response.data.isSuccess) {
+        const updatedDates = new Array(32).fill(null); // 초기화
+        response.data.result.forEach((answerId, index) => {
+          if (answerId !== 0) {
+            const fullDate = new Date(year, month - 1, index + 1); // 날짜 계산
+            updatedDates[index] = { date: fullDate, answerId }; // 날짜와 answerId를 객체로 저장
+          }
+        });
+        setMarkedDates(updatedDates);
+        console.log("달력 정보 가져오기 성공");
+      } else {
+        console.error("달력 정보 가져오기 실패", response.data.message);
+      }
+    } catch (error) {
+      console.error("달력 정보 가져오기 요청 오류", error);
+    }
+  };
 
-            {isNewReport && (
-                <div className="fixed inset-0 z-50 flex flex-col items-center justify-center bg-black bg-opacity-50">
-                    <div
-                        className="relative flex flex-col items-center text-xl font-semibold text-center text-white font-paperlogy-heading">
-                        <div className="mt-6 mb-6 animate-floating">
-                            <p>
-                                잠깐! <br/>
-                                새로운 분석 리포트가 도착했습니다! <br/>
-                                상단의 종 아이콘을 눌러 확인해주세요!
-                            </p>
-                        </div>
-                        <button
-                            className="px-8 py-4 mt-4 text-lg font-semibold text-black rounded-full bg-primary-20"
-                            onClick={() => handleCloseNotification()}
-                        >
-                            확인하러 가기
-                        </button>
-                    </div>
-                </div>
-            )}
+  useEffect(() => {
+    const year = selectedDate.getFullYear();
+    const month = selectedDate.getMonth() + 1;
+    fetchCalendarData(year, month);
+  }, [selectedDate]);
 
-            <Header/>
-            <div className="flex flex-col items-center flex-grow p-4">
-                <div className="flex justify-center items-center w-full pt-4 mb-[-4px]">
-                    <h1 className="px-4 py-2 text-base font-bold rounded-lg text-grayscale-90 bg-grayscale-10">
-                        날짜를 선택하면 당일 피드백을 확인할 수 있어요
-                    </h1>
-                </div>
+  const getTileClassName = ({ date, view }) => {
+    const today = new Date();
+    const isWeekend = date.getDay() === 0 || date.getDay() === 6; // 주말 (0: 일요일, 6: 토요일)
 
-                <div className="mt-4 text-sm font-bold text-center text-grayscale-70">
-                    스피치를 진행한 날은{" "}
-                    <img
-                        src="/images/peach3.png"
-                        alt="복숭아 이미지"
-                        className="inline-block w-6 h-6"
-                    />{" "}
-                    로 표시됩니다!
-                </div>
+    if (view === "month") {
+      // 오늘 날짜
+      if (
+        date.getDate() === today.getDate() &&
+        date.getMonth() === today.getMonth() &&
+        date.getFullYear() === today.getFullYear()
+      ) {
+        return "text-pink-700 font-semibold";
+      }
+      // 선택된 날짜
+      if (
+        date.getDate() === selectedDate.getDate() &&
+        date.getMonth() === selectedDate.getMonth() &&
+        date.getFullYear() === selectedDate.getFullYear()
+      ) {
+        return "font-semibold";
+      }
+      if (isWeekend) {
+        return "text-black"; // 검정색 텍스트
+      }
+    }
+    return "hover:bg-grayscale-20 transition";
+  };
 
-                <div className="w-full max-w-[400px] mt-4">
-                    <Calendar
-                        onChange={handleDateChange} // 날짜 선택 시 상태 업데이트
-                        onActiveStartDateChange={({activeStartDate}) => {
-                            const year = activeStartDate.getFullYear();
-                            const month = activeStartDate.getMonth() + 1;
-                            fetchCalendarData(year, month);
-                        }}
-                        value={selectedDate}
-                        className="w-full calendar-custom"
-                        tileClassName={getTileClassName}
-                        formatDay={(locale, date) => date.getDate()}
-                        tileContent={({date, view}) => {
-                            if (view === "month") {
-                                const markedData = markedDates[date.getDate() - 1];
-                                const isMarked =
-                                    markedData &&
-                                    markedData.date.toDateString() === date.toDateString() && // 날짜 비교 (문자열 형태)
-                                    markedData.answerId !== 0;
-                                if (isMarked) {
-                                    console.log(date);
-                                    console.log(markedData.date)
-                                    return (
-                                        <div className="relative flex items-center justify-center px-4 py-2">
-                                            <div
-                                                className="tile-background"
-                                                style={{backgroundImage: "url('/images/peach3.png')"}}
-                                            ></div>
-                                            <span className="date-number">{date.getDate()}</span>
-                                        </div>
-                                    );
-                                } else {
-                                    return (
-                                        <div className="flex items-center justify-center">
-                                            <span>{date.getDate()}</span>
-                                        </div>
-                                    );
-                                }
-                            }
-                        }}
-                    />
-                </div>
+  const handleCloseNotification = () => {
+    setIsNewReport(false);
+  };
+
+  return (
+    <div className="w-full h-full max-w-[500px] mx-auto flex flex-col bg-white">
+      {isNewReport && (
+        <div className="fixed inset-0 z-50 flex flex-col items-center justify-center bg-black bg-opacity-50">
+          <div className="relative flex flex-col items-center text-xl font-semibold text-center text-white font-paperlogy-heading">
+            <div className="mt-6 mb-6 animate-floating">
+              <p>
+                🖐🏻 잠깐 🖐🏻 <br />
+                새로운 분석 리포트가 도착했습니다! <br />
+                상단의 종 아이콘을 눌러 확인해주세요!
+              </p>
             </div>
-            <NavBar/>
+            <button
+              className="px-8 py-4 mt-4 text-lg font-semibold text-black rounded-full bg-primary-20"
+              onClick={handleCloseNotification}
+            >
+              확인하러 가기
+            </button>
+          </div>
+        </div>
+      )}
+      <Header />
+      <div className="flex flex-col items-center flex-grow p-4">
+        <div className="flex justify-center items-center w-full pt-4 mb-[-4px]">
+          <h1 className="px-4 py-2 text-base font-bold rounded-lg text-grayscale-90 bg-grayscale-10">
+            날짜를 선택하면 당일 피드백을 확인할 수 있어요
+          </h1>
+        </div>
 
-            {/* 리액트 캘린더 라이브러리 커스텀 */}
-            <style jsx>{`
-                .calendar-custom .react-calendar__month-view__weekdays__weekday abbr {
-                    text-decoration: none;
-                }
         <div className="mt-4 text-sm font-bold text-center text-grayscale-70">
           스피치를 진행한 날은{" "}
           <img
@@ -217,13 +149,13 @@ const CalendarPage = () => {
                   markedData.date.toDateString() === date.toDateString() && // 날짜 비교 (문자열 형태)
                   markedData.answerId !== 0;
                 if (isMarked) {
-                  console.log(date);
-                  console.log(markedData.date);
                   return (
                     <div className="relative flex items-center justify-center px-4 py-2">
                       <div
                         className="tile-background"
-                        style={{ backgroundImage: "url('/images/peach3.png')" }}
+                        style={{
+                          backgroundImage: "url('/images/peach3.png')",
+                        }}
                       ></div>
                       <span className="date-number">{date.getDate()}</span>
                     </div>
@@ -241,7 +173,6 @@ const CalendarPage = () => {
         </div>
       </div>
       <NavBar />
-
       {/* 리액트 캘린더 라이브러리 커스텀 */}
       <style jsx>{`
         .calendar-custom .react-calendar__month-view__weekdays__weekday abbr {
--- src/pages/Statistics/index.js
+++ src/pages/Statistics/index.js
@@ -1,4 +1,4 @@
-import React, {useContext, useState} from "react";
+import React, { useContext, useState } from "react";
 import Header from "../../components/Header";
 import NavBar from "../../components/NavBar";
 import {
@@ -10,8 +10,8 @@ import {
   YAxis,
 } from "recharts";
 import instance from "../../axios/TokenInterceptor";
-import {SPRING_API_URL} from "../../constants/api";
-import {NotificationContext} from "../../context/NotificationProvider";
+import { SPRING_API_URL } from "../../constants/api";
+import { NotificationContext } from "../../context/NotificationProvider";
 
 const StatisticsPage = () => {
   const [activeKey, setActiveKey] = useState("");
@@ -20,14 +20,11 @@ const StatisticsPage = () => {
   const [statisticsData, setStatisticsData] = useState([]);
   const [level, setLevel] = useState(5);
 
-    const {
-        isNewReport,
-        setIsNewReport,
-    } = useContext(NotificationContext);
+  const { isNewReport, setIsNewReport } = useContext(NotificationContext);
 
-    const handleDragScroll = (event) => {
-        const isTouch = event.type === "touchstart" || event.type === "touchmove";
-        const clientX = isTouch ? event.touches[0].clientX : event.clientX;
+  const handleDragScroll = (event) => {
+    const isTouch = event.type === "touchstart" || event.type === "touchmove";
+    const clientX = isTouch ? event.touches[0].clientX : event.clientX;
 
     if (scrollRef.current) {
       scrollRef.current.scrollLeft += scrollPosition - clientX;
@@ -80,83 +77,52 @@ const StatisticsPage = () => {
         } else if (response.data.code === "STATISTICS4001") {
           console.error("난이도별 통계 데이터 받아오기 실패");
         }
-    };
-
-    const handleCloseNotification = () => {
-        setIsNewReport(false);
+        console.log("난이도별 통계 데이터 받아오기 성공");
+      } else {
+        console.error("난이도별 통계 데이터 받아오기 오류");
+        console.log(response.data.code);
+        console.log(response.data.message);
+      }
+    } catch (error) {
+      console.error("난이도별 통계 데이터 받아오기 오류");
     }
+  };
 
-    return (
-        <div className="w-full h-full max-w-[500px] mx-auto flex flex-col bg-white">
-            {isNewReport && (
-                <div className="fixed inset-0 z-50 flex flex-col items-center justify-center bg-black bg-opacity-50">
-                    <div
-                        className="relative flex flex-col items-center text-xl font-semibold text-center text-white font-paperlogy-heading">
-                        <div className="mt-6 mb-6 animate-floating">
-                            <p>
-                                잠깐! <br/>
-                                새로운 분석 리포트가 도착했습니다! <br/>
-                                상단의 종 아이콘을 눌러 확인해주세요!
-                            </p>
-                        </div>
-                        <button
-                            className="px-8 py-4 mt-4 text-lg font-semibold text-black rounded-full bg-primary-20"
-                            onClick={() => handleCloseNotification()}
-                        >
-                            확인하러 가기
-                        </button>
-                    </div>
-                </div>
-            )}
-
-            <Header/>
-            <div className="flex flex-col items-center flex-grow p-4">
-                <p className="px-4 py-2 my-4 text-base rounded-md text-grayscale-90 font-paperlogy-title bg-grayscale-10">
-                    최근 기록을 확인하고 나의 말하기 습관을 분석해보세요.
-                </p>
-                <p className="mb-4 text-sm text-grayscale-90 font-paperlogy-title">
-                    좌우로 스크롤 하면 이전 기록을 한 번에 확인할 수 있어요
-                </p>
-                {/* 그래프 영역 */}
-                <div className="relative flex w-full">
-                    {/* 난이도 버튼 */}
-                    <div className="flex flex-col items-center justify-center mr-4 space-y-4">
-                        {[1, 2, 3].map((level) => (
-                            <button
-                                key={level}
-                                onClick={() => {
-                                    setLevel(level);
-                                    handleLevelButton(level);
-                                }}
-                                className={buttonStyle(level)}
-                            >
-                                {level}
-                            </button>
-                        ))}
-                        <button
-                            onClick={() => {
-                                setLevel(4);
-                                handleWholeButton();
-                            }}
-                            className={buttonStyle(4)}
-                        >
-                            전체
-                        </button>
-                    </div>
+  const handleCloseNotification = () => {
+    setIsNewReport(false);
+  };
 
   return (
     <div className="w-full h-full max-w-[500px] mx-auto flex flex-col bg-white">
+      {isNewReport && (
+        <div className="fixed inset-0 z-50 flex flex-col items-center justify-center bg-black bg-opacity-50">
+          <div className="relative flex flex-col items-center text-xl font-semibold text-center text-white font-paperlogy-heading">
+            <div className="mt-6 mb-6 animate-floating">
+              <p>
+                잠깐! <br />
+                새로운 분석 리포트가 도착했습니다! <br />
+                상단의 종 아이콘을 눌러 확인해주세요!
+              </p>
+            </div>
+            <button
+              className="px-8 py-4 mt-4 text-lg font-semibold text-black rounded-full bg-primary-20"
+              onClick={handleCloseNotification}
+            >
+              확인하러 가기
+            </button>
+          </div>
+        </div>
+      )}
+
       <Header />
-      <div className="flex-1 w-full px-4 pt-3 pb-5 mx-auto overflow-y-auto bg-white scrollbar-hide">
-        <p className="p-2 my-4 text-base text-center rounded-md text-grayscale-90 font-paperlogy-title bg-grayscale-10">
-          최근 기록을 확인하고 말하기 습관을 분석해보세요
+      <div className="flex flex-col items-center flex-grow p-4">
+        <p className="px-4 py-2 my-4 text-base rounded-md text-grayscale-90 font-paperlogy-title bg-grayscale-10">
+          최근 기록을 확인하고 나의 말하기 습관을 분석해보세요.
         </p>
-        <p className="mb-4 text-sm text-center text-grayscale-90 font-paperlogy-title">
+        <p className="mb-4 text-sm text-grayscale-90 font-paperlogy-title">
           좌우로 스크롤 하면 이전 기록을 한 번에 확인할 수 있어요
         </p>
-        {/* 그래프 영역 */}
         <div className="relative flex w-full">
-          {/* 난이도 버튼 */}
           <div className="flex flex-col items-center justify-center mr-4 space-y-4">
             {[1, 2, 3].map((level) => (
               <button
@@ -181,7 +147,6 @@ const StatisticsPage = () => {
             </button>
           </div>
 
-          {/* 스크롤 가능 영역 */}
           <div
             className="relative flex-grow overflow-x-auto border scrollbar-hide rounded-3xl"
             ref={scrollRef}
@@ -192,8 +157,8 @@ const StatisticsPage = () => {
             onTouchMove={handleDragScroll}
             onTouchEnd={() => setScrollPosition(0)}
           >
-            {/* 그래프 */}
             <div className="min-w-[800px] pr-4">
+              {/* 그래프 */}
               <ResponsiveContainer width="100%" height={300}>
                 <LineChart
                   data={statisticsData}
@@ -213,7 +178,6 @@ const StatisticsPage = () => {
                     axisLine={{ stroke: "#ccc" }}
                     tickLine={false}
                   />
-                  {/* 그래프 선 */}
                   <Line
                     type="monotone"
                     dataKey={activeKey}
@@ -266,15 +230,15 @@ const StatisticsPage = () => {
         </div>
       </div>
       <NavBar />
+
       {/* 그래프 좌우 스크롤바 숨김 처리 */}
       <style jsx>{`
         .scrollbar-hide {
-          -ms-overflow-style: none; /* IE and Edge */
-          scrollbar-width: none; /* Firefox */
+          -ms-overflow-style: none;
+          scrollbar-width: none;
         }
-
         .scrollbar-hide::-webkit-scrollbar {
-          display: none; /* Chrome, Safari, and Opera */
+          display: none;
         }
       `}</style>
     </div>
